06b3679a533a12fe9ded828df6aa36e6f6fe4c0c
Rupinder <rupinder3972@gmail.com>
2024-05-14 22:22:26 +0530

    fixed build issues

--- src/Component/TopLayout.js
+++ src/Component/TopLayout.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
 import React from 'react';
+import { useState, useEffect } from 'react';
 import {Link} from 'react-router-dom';
 import axios from 'axios'; 
 import { useNavigate } from "react-router-dom";
--- src/PropertiesFilter - Copy.js
+++ src/PropertiesFilter - Copy.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React,{ useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom'; 
 import { useForm } from "react-hook-form";
 import axios from 'axios'; 
